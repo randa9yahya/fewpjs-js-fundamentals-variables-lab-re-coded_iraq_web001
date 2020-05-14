539dd9b41fb7fa6e73d7e894f48077046f3d8e03
randa yahya <randaalrayan96@gmial.com>
2020-05-14 17:31:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+ 
+let mostProfitableNeighborhood = 'Chelsea';
+mostProfitableNeighborhood = 'Upper West Side';
+let companyCeo = 'Susan Smith';
+companyCeo = 'Lauren Hart';
